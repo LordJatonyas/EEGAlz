a03f94d593027c3067cdb7bc91a7496f940572df
Lee Chih Jung <john.12012000@gmail.com>
2024-04-19 16:48:04 +0100

    Some updates

--- src/testing.jl
+++ src/testing.jl
@@ -14,7 +14,7 @@ begin
 end
 
 # ╔═╡ 3c22c13d-437b-42f1-8532-c9888cb3f0ff
-using CSV, DataFrames, LinearAlgebra, Statistics, Plots
+using CSV, DataFrames, LinearAlgebra, Statistics, Plots, SeisNoise, DSP
 
 # ╔═╡ 5fee6ea7-34b9-4144-a998-0079b385856f
 begin
@@ -23,20 +23,11 @@ begin
 	first(df, 2)
 end
 
-# ╔═╡ a255a0c1-31cf-446f-82ea-827326805b1b
-begin
-	function hist_ends(channel, tail_length, channel_name)
-		largest_values = sort(channel, rev=true)[1:tail_length]
-		smallest_values = sort(channel)[1:tail_length]
-		pS = histogram(smallest_values, title=channel_name * " - Smallest Values")
-		pL = histogram(largest_values, title=channel_name * " - Largest Values")
-		plot(pS, pL, layout=(1,2), legend=false)
-	end
-end
-
 # ╔═╡ b7d81afb-dfb3-42e1-96e9-f92fcf7f0cd1
+# ╠═╡ disabled = true
+#=╠═╡
 begin
-	# there don't seem to be anomalous data points! So normalise as per usual
+	# we are assuming that the EEG recording is good
 	function normalise_channel(channel)
 		largest_value = maximum(channel)
 		smallest_value = minimum(channel)
@@ -49,22 +40,40 @@ begin
 		end
 	end
 end
+  ╠═╡ =#
 
 # ╔═╡ 1b347b1e-39da-4038-b6c6-461e6163007c
+# ╠═╡ disabled = true
+#=╠═╡
 begin
 	for i in names(df)
+		detrend!(df[!, i])
+		mute!(df[!, i])
 		normalise_channel(df[!, i])
 	end
 end
+  ╠═╡ =#
+
+# ╔═╡ 1010bc25-98a7-4836-a821-cb554bb38c34
+begin
+Y = periodogram(df.P3; fs=500)
+plot(Y.freq, 2 * DSP.pow2db.(Y.power), legend=false)
+end
 
 # ╔═╡ 921db00e-2942-4132-bd37-bc37f3023f9b
-
+# ╠═╡ disabled = true
+#=╠═╡
+begin
+Y = periodogram(df.Fp1; fs=500)
+plot(Y.freq, 2 * DSP.pow2db.(Y.power), legend=false)
+end
+  ╠═╡ =#
 
 # ╔═╡ Cell order:
 # ╠═70f3e92c-2339-42ea-9c73-9ab07730f569
 # ╠═3c22c13d-437b-42f1-8532-c9888cb3f0ff
 # ╠═5fee6ea7-34b9-4144-a998-0079b385856f
-# ╠═a255a0c1-31cf-446f-82ea-827326805b1b
 # ╠═b7d81afb-dfb3-42e1-96e9-f92fcf7f0cd1
 # ╠═1b347b1e-39da-4038-b6c6-461e6163007c
 # ╠═921db00e-2942-4132-bd37-bc37f3023f9b
+# ╠═1010bc25-98a7-4836-a821-cb554bb38c34
